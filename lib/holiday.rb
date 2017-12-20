cfdcd9913020ffe043ef475351798672117d064e
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-20 17:30:08 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season,holidays|
     puts "#{season.capitalize}:"
     holidays.each do |holiday,supplies|
-      puts "  #{holiday.capitalize}:"
+      first_var="  #{holiday.capitalize}:"
       supplies.each do |items|
         print items
       end
