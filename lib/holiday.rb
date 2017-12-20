f04de597a8e84752b9b314f4e1b555b1e72d0193
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-20 17:18:04 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season,holiday|
+    puts season
+    holiday.each do |supplies|
 
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
