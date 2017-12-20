a537015fc00ba57177bd2f328bd8b30dab342fcc
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-20 18:03:59 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,5 +78,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  array1=[]
+  holiday.hash.collect do |season,holidays|
+    holidays.collect do |holiday,supplies|
+      if supplies.include?("BBQ")
+        holiday
+      end
 end
